471223609ad1fa7346e725b597fe6b92d69dea27
Declan <fluffyjeb@gmail.com>
2018-03-19 18:25:45 +0000

    Fixed

--- bot.js
+++ bot.js
@@ -13,16 +13,16 @@ client.on('message', msg => {
   const command = args.shift().toLowerCase();
   if (command === "help")
   {
-    if (args[0] === "mcserver")
+    if (args[0] === "mcs")
      {
-       msg.channel.send("This prints a message with informaion about the target minecraft server.\nUsage: !mcserver <ipaddress>:[port]");
+       msg.channel.send("This prints a message with informaion about the target minecraft server.\nUsage: !mcs <ipaddress>:[port]");
      }
      else
      {
-       msg.channel.send("Help for Wishbot:\n- help: Displays this message.\n- mcserver: Displays information about a target minecraft server. Usage: !mcserver <ipaddress>:[port]")
+       msg.channel.send("Help for Wishbot:\n- help: Displays this message.\n- mcs: Displays information about a target minecraft server. Usage: !mcs <ipaddress>:[port]")
      }
   }
-  if (command === "mcserver")
+  if (command === "mcs")
   {
       var url = ("https://mcapi.us/server/status?ip="+args[0]);
       getJSON(url, function(error,response)
@@ -33,14 +33,14 @@ client.on('message', msg => {
             }
             else
             {
-              if (response.last_updated)
+              if (response.last_online)
               {
                   var dayssinceonline = Math.round((((new Date()).getTime()) - response.last_updated)/(60*60*24));
                   msg.channel.send(args[0] + " is offline! :anger:\nIt was last recorded online " + dayssinceonline +" days ago.");
               }
               else
               {
-                msg.channel.send(args[0] + " is offline! :anger:\nAccording to our records this server has never been online. :sob:");
+                msg.channel.send(args[0] + " is offline! :anger:\nAccording to MCApi's records this server has never been online. :sob:");
               }
             }
       });
